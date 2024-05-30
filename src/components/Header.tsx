d1a51e5a743d3ebdc3d62d91317af453ea802d95
gia-grigalashvili <grigalagia07@gmail.com>
2024-05-30 21:56:34 +0400

    made body style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <Headerdiv>
       <h1>devfinder</h1>
-      <div>
+      <div className="nightmode">
         <h1>LIGHT</h1>
         <img src={Moon} alt="" />
       </div>
@@ -16,5 +16,26 @@ const Headerdiv = styled.div`
   justify-content: center;
   align-items: center;
   gap: 121px;
+  h1 {
+    color: #222731;
+    font-family: "Space Mono";
+    font-size: 26px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+  nightmode {
+    display: flex;
+    h1 {
+      color: #4b6a9b;
+      text-align: right;
+      font-family: "Space Mono";
+      font-size: 13px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+      letter-spacing: 2.5px;
+    }
+  }
 `;
 export default Header;
