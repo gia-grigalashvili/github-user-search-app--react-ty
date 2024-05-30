05ea616a1e7438315a3580831f004224710a53b7
gia-grigalashvili <grigalagia07@gmail.com>
2024-05-30 21:54:05 +0400

    import moon img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,15 @@
 import styled from "styled-components";
-
+import Moon from "/public/images/icon-moon.svg";
 function Header() {
-  return <Headerdiv></Headerdiv>;
+  return (
+    <Headerdiv>
+      <h1>devfinder</h1>
+      <div>
+        <h1>LIGHT</h1>
+        <img src={Moon} alt="" />
+      </div>
+    </Headerdiv>
+  );
 }
 const Headerdiv = styled.div`
   display: inline-flex;
