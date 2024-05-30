bbc2c6fd0187c161961c3d51cc91bc75f2af2b39
gia-grigalashvili <grigalagia07@gmail.com>
2024-05-30 21:51:29 +0400

    made cotainer.tsx and header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function Header() {
+  return <Headerdiv></Headerdiv>;
+}
+const Headerdiv = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 121px;
+`;
+export default Header;
